ff9f7607d9dca2d2184d34e67925b15f14a62000
Johannes Hartmann <Johannes.Hartmann.Calw@googlemail.com>
2019-01-25 21:44:45 +0100

    Add unlinking and destroying

--- Assets/Sources/Views/View.cs
+++ Assets/Sources/Views/View.cs
@@ -2,10 +2,21 @@ using UnityEngine;
 using Entitas;
 using Entitas.Unity;
 
-public class View : MonoBehaviour, IView
+public class View : MonoBehaviour, IView, ICoreDestroyedListener
 {
     public virtual void Link(CoreEntity entity)
     {
         gameObject.Link(entity);
+    }
+
+    public void OnDestroyed(CoreEntity entity)
+    {
+        DestroyView();
+    }
+
+    public virtual void DestroyView()
+    {
+        gameObject.Unlink();
+        Destroy(gameObject);
     }
 }
