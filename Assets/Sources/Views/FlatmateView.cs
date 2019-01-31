871c077e25f94445a070a3f83e0f921dd4bc72cc
Johannes Hartmann <Johannes.Hartmann.Calw@googlemail.com>
2019-01-31 12:29:09 +0100

    Added more positions for activities

--- Assets/Sources/Views/FlatmateView.cs
+++ Assets/Sources/Views/FlatmateView.cs
@@ -38,17 +38,44 @@ public class FlatmateView : View, ICurrentRoomListener
                 {
                     transform.position = new Vector2(5.02f, 2.67f);
                 }
-                else
+                else if (entity.hasActiveAction && entity.activeAction.value == Action.WATCH_TV)
                 {
                     transform.position = new Vector2(6.82f, 0.31f);
                 }
+                else
+                {
+                    transform.position = new Vector2(6.82f, 2f);
+                }
                 break;
             case 1: // Kitchen
-                transform.position = new Vector2(1.92f, 3.87f);
+                if (entity.hasActiveAction && entity.activeAction.value == Action.DO_THE_DISHES)
+                {
+                    transform.position = new Vector2(1.92f, 3.87f);
+                }
+                else if (entity.hasActiveAction && entity.activeAction.value == Action.EAT)
+                {
+                    transform.position = new Vector2(3.8f, 0.5f);
+                }
+                else
+                {
+                    transform.position = new Vector2(3f, 3f);
+                }
                 break;
 
             case 2: // Bathroom
-                transform.position = new Vector2(-0.74f, -3f);
+                if (entity.hasActiveAction && entity.activeAction.value == Action.GO_TO_TOILET)
+                {
+                    transform.position = new Vector2(0.2f, -4.2f);
+                }
+                else if (entity.hasActiveAction && entity.activeAction.value == Action.SHOWER)
+                {
+                    transform.position = new Vector2(0.2f, -3f);
+                }
+                else
+                {
+                    transform.position = new Vector2(-1.5f, -3f);
+                }
+
                 break;
             default:
                 transform.position = new Vector2(0f, 0f);
